3a67cc4efa7143e35fa34f2745afcfbcd1d7608e
David Baik <davidbaik1001@gmail.com>
2025-12-14 16:01:09 -0600

    Fixes for vercel 55. DB

---
+++ mc-shipment-tracker/src/app/resources/page.tsx
@@ -0,0 +1,8 @@
+import ResourcesForm from './resources-form';
+
+export default async function Resources() {
+
+  return (
+    <ResourcesForm />
+  );
+}
---
+++ mc-shipment-tracker/src/app/resources/resources-form.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function ResourcesForm() {
+
+    return (
+        <div>
+            Resources Stuff Here
+        </div>
+    );
+}
